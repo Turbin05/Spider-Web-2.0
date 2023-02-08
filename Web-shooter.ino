2f34f9d3a469d9070525a2429d31181fabee66e9
Tural Binnatov <89636414+Turbin05@users.noreply.github.com>
2023-02-08 16:58:13 +0100

    Update Web-shooter.ino

--- Web-shooter.ino
+++ Web-shooter.ino
@@ -1,22 +1,19 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
-
+Servo myservo;  
+int pos = 0;    
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9); 
 }
 
 void loop() {
-  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 90 degrees instead of 180 degress
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+  for (pos = 0; pos <= 90; pos += 1) { // Går från 0 grader till 90 grader istället för 180 grader
+    // Läggs till med 1 grad
+    myservo.write(pos);
+    delay(15);                      
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+  for (pos = 180; pos >= 0; pos -= 1) { // Går från 180 grader till 0 grader
+    myservo.write(pos);
+    delay(15);                       
   }
 }
